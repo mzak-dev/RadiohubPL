06175a78923298702c333fb7f20cc16fa56cc5f5
EntitledGroup <54439704+EntitledGroup@users.noreply.github.com>
2019-08-29 19:14:46 +0200

    Add files via upload

---
+++ sampleData.js
@@ -0,0 +1,83 @@
+const shortLoremIpsum = `Opis[Placeholder]`;
+const sampleData = {};
+const longLoremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
+  non enim praesent elementum facilisis leo vel. Risus at ultrices mi
+  tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
+  tellus. Convallis convallis tellus id interdum velit laoreet id donec
+  ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
+  suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
+  quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
+  proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
+  tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
+  varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
+  Lorem donec massa sapien faucibus et molestie ac.`;
+
+// This class holds sample data used by some generated pages to show how they can be used.
+// TODO Web Template Studio: Delete this file once your app is using real data.
+sampleData.textAssets = [
+  {
+    shortDescription: shortLoremIpsum,
+    longDescription: longLoremIpsum,
+    title: "Radio Zet",
+    status: "Online",
+    radiocover: "https://polskafm.com/sites/default/files/radio/logos/logo-rmf-maxxx.png",
+    audioUrl: "http://redir.atmcdn.pl/sc/o2/Eurozet/live/audio.livx?audio=5",
+    orderDate: new Date(2017, 5, 24).toDateString(),
+    id: 1
+  },
+  {
+    shortDescription: shortLoremIpsum,
+    longDescription: longLoremIpsum,
+    title: "Eska",
+    status: "Online",
+    radiocover: "https://polskafm.com/sites/default/files/radio/logos/logo-rmf-maxxx.png",
+    audioUrl: "http://31.192.216.8/rmf_party",
+    orderDate: new Date(2017, 5, 24).toDateString(),
+    id: 2
+  },
+  {
+    shortDescription: shortLoremIpsum,
+    longDescription: longLoremIpsum,
+    title: "MeloRadio",
+    status: "Online",
+    radiocover: "https://polskafm.com/sites/default/files/radio/logos/logo-rmf-maxxx.png",
+    audioUrl: "http://mel0101.cdn.eurozet.pl:8800/mel-net.mp3",
+    orderDate: new Date(2017, 6, 3).toDateString(),
+    id: 3
+  },
+  {
+    shortDescription: shortLoremIpsum,
+    longDescription: longLoremIpsum,
+    title: "Radio Złote Przeboje",
+    status: "Online",
+    radiocover: "https://polskafm.com/sites/default/files/radio/logos/logo-rmf-maxxx.png",
+    audioUrl: "http://poznan5-6.radio.pionier.net.pl:8000/tuba9-1.mp3",
+    orderDate: new Date(2017, 6, 5).toDateString(),
+    id: 4
+  },
+  {
+    shortDescription: shortLoremIpsum,
+    longDescription: longLoremIpsum,
+    title: "RMF MAXXX",
+    status: "Online",
+    radiocover: "https://polskafm.com/sites/default/files/radio/logos/logo-rmf-maxxx.png",
+    audioUrl: "http://195.150.20.6/rmf_maxxx",
+    orderDate: new Date(2017, 6, 7).toDateString(),
+    id: 5
+  },
+  {
+    shortDescription: shortLoremIpsum,
+    longDescription: longLoremIpsum,
+    title: "RMF FM",
+    status: "Online",
+    radiocover: "https://polskafm.com/sites/default/files/radio/logos/logo-rmf-maxxx.png",
+    audioUrl: "http://195.150.20.6/rmf_fm",
+    orderDate: new Date(2017, 6, 7).toDateString(),
+    id: 6
+  },
+
+];
+
+
+module.exports = sampleData;
